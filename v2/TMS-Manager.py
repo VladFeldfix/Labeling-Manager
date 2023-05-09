c081964a7164019d845024dbab97a327a2aa922d
Vlad Feldfix <62237119+VladFeldfix@users.noreply.github.com>
2023-05-09 16:07:45 +0300

    Add files via upload

--- v2/TMS-Manager.py
+++ v2/TMS-Manager.py
@@ -13,6 +13,7 @@ from PersonalAssistant import *
 from PersonalAssistant import FIELD
 import os
 import sqlite3
+import shutil
 
 class main:
     def __init__(self):
@@ -25,18 +26,21 @@ class main:
         self.cur = self.con.cursor()
 
         # create database tables
-        self.cur.execute("CREATE TABLE IF NOT EXISTS tms (part_number VARCHAR(50), size VARCHAR(50), color VARCHAR(50))")
+        self.cur.execute("CREATE TABLE IF NOT EXISTS tms (part_number VARCHAR(50), size VARCHAR(50))")
         self.cur.execute("CREATE TABLE IF NOT EXISTS tms_inventory (box_number INT, part_number VARCHAR(50))")
-        self.cur.execute("CREATE VIEW IF NOT EXISTS dbview AS SELECT tms_inventory.box_number, tms.part_number, tms.size, tms.color FROM tms, tms_inventory WHERE tms.part_number = tms_inventory.part_number")
+        self.cur.execute("CREATE VIEW IF NOT EXISTS dbview AS SELECT tms_inventory.box_number, tms.part_number, tms.size FROM tms, tms_inventory WHERE tms.part_number = tms_inventory.part_number")
         self.con.commit()
 
         # load settings
         self.labels_location = self.pa.get_setting("TMS Location")
-        if os.path.isdir(self.lbaels_templates_location):
+        if not os.path.isdir(self.labels_location):
             self.pa.fatal_error("Missing folder: "+self.labels_location)
         self.lbaels_templates_location = self.labels_location+"/_Templates_"
-        if os.path.isdir(self.lbaels_templates_location):
-            self.pa.fatal_error("Missing folder: "+self.labels_location+"/_Templates_")
+        if not os.path.isdir(self.lbaels_templates_location):
+            self.pa.fatal_error("Missing folder: "+self.lbaels_templates_location)
+        
+        # create tms database
+        self.create_tms_database()
 
         # display database
         self.display_database()
@@ -50,41 +54,77 @@ class main:
         self.pa.run()
 
     def display_database(self):
-        self.pa.display_database(self.db_location, "dbview", "box_number")
+        if not self.pa.database_is_displayed():
+            self.pa.display_database(self.db_location, "dbview", "box_number")
+        else:
+            self.pa.update_database()
+
+    def create_tms_database(self):
+        error = False
+        tmss_to_add = []
+        for root, dirs, files in os.walk(self.lbaels_templates_location):
+            for file in files:
+                if ".btw" in file:
+                    # this is a template
+                    tmp = file.split(".")
+                    if len(tmp) == 2:
+                        tmp = tmp[0].split(" ")
+                        if len(tmp) == 4:
+                            # 0 TMS
+                            # 1 TEMPLATE
+                            # 2 PART NUMBER
+                            # 3 SIZE
+                            part_number = tmp[2]
+                            size = tmp[3]
+                            tmss_to_add.append((part_number,size))
+        if not error:
+            self.cur.execute("DELETE FROM tms")
+            for x in tmss_to_add:
+                part_number = x[0]
+                size = x[1]
+                self.cur.execute("INSERT INTO tms (part_number, size) VALUES ('"+part_number+"', '"+size+"')")
+            self.con.commit()
     
     def print_labels(self):
         ## PRINT LABELS allows you to create and print new TMS files for a given work order
 
         # get work order
-        work_order = self.pa.input("Insert WORK ORDER") or "000000"
+        work_order = self.pa.input("Insert WORK ORDER")
         error = False
         
-        # search for work order
-        for root, dirs, files in os.walk(self.labels_location):
-            for file in files:
-                if work_order in file:
-                    self.pa.error("This WORK ORDER have been printed before")
-                    error = True
+        if work_order != "":
+            # search for work order
+            for root, dirs, files in os.walk(self.labels_location):
+                for file in files:
+                    if work_order in file:
+                        self.pa.error("This WORK ORDER have been printed before")
+                        error = True
+        else:
+            self.pa.error("Invalid WORK ORDER")
+            error = True
         
         if not error:
             # get part number
-            part_number = self.pa.input("Insert product PART NUMBER").upper() or "EMPTY"
+            product_part_number = self.pa.input("Insert product PART NUMBER").upper()
+            if product_part_number == "":
+                self.pa.error("Invalid product PART NUMBER")
+                error = True
         
         if not error:
             # see if part number is in the database
-            self.cur.execute("SELECT * FROM products WHERE part_number = '"+part_number+"'")
+            self.cur.execute("SELECT * FROM products WHERE part_number = '"+product_part_number+"'")
             fetched_values = self.cur.fetchall()
             if len(fetched_values) == 0:
-                self.pa.error("This PART NUMBER is not in the database")
+                self.pa.error("This PART NUMBER is not in the database. Go to Product Manager and create a new product")
                 error = True
 
         if not error:
             # see if folder exists, create if not
-            if not os.path.isdir(self.labels_location+"/"+part_number):
-                os.makedirs(self.labels_location+"/"+part_number)
+            if not os.path.isdir(self.labels_location+"/"+product_part_number):
+                os.makedirs(self.labels_location+"/"+product_part_number)
             
             # open script
-            script_file_location = self.labels_location+"/"+part_number+"/script.txt"
+            script_file_location = self.labels_location+"/"+product_part_number+"/script.txt"
             if not os.path.isfile(script_file_location):
                 # add balloons
                 go = True
@@ -95,17 +135,19 @@ class main:
                 while go:
                     if not error:
                         balloon = self.pa.input("Insert BALLOON number according to BOM") or "0"
-                        part_number = self.pa.input("Insert LABEL PART NUMBER for balloon: "+balloon) or "NONE"
-                        self.cur.execute("SELECT * FROM tms WHERE part_number = '"+part_number+"'")
+                        label_part_number = self.pa.input("Insert LABEL PART NUMBER for balloon: "+balloon) or "NONE"
+                        self.cur.execute("SELECT * FROM tms WHERE part_number = '"+label_part_number+"'")
                         fetched_values = self.cur.fetchall()
                         if len(fetched_values) == 0:
-                            self.pa.error("This TMS PART NUMBER is not in the database")
+                            self.pa.error("This TMS PART NUMBER is not in the database. Go to Inventory")
                             error = True
                         else:
-                            balloons[balloon] = part_number
+                            balloons[balloon] = label_part_number
                             balloons_options.append(balloon)
                             if not self.pa.question("Is there another BALLOON?"):
                                 go = False
+                    else:
+                        go = False
                 if not error:
                     # add label names
                     go = True
@@ -118,16 +160,18 @@ class main:
                             labels[label_name] = balloon
                             if not self.pa.question("Is there another LABEL?"):
                                 go = False
+                    else:
+                        go = False
                 
                 # write script
                 if not error:
                     file = open(script_file_location, "w")
-                    file.write("BALLOONS")
-                    for balloon, part_number in balloons.items():
-                        file.write(balloon+":"+part_number)
-                    file.write("LABELS")
+                    file.write("BALLOONS\n")
+                    for balloon, label_part_number in balloons.items():
+                        file.write(balloon+":"+label_part_number+"\n")
+                    file.write("LABELS\n")
                     for label_name, balloon in labels.items():
-                        file.write(label_name+":"+balloon)
+                        file.write(label_name+":"+balloon+"\n")
                     file.close()
             
             # read script 
@@ -155,17 +199,156 @@ class main:
                                     error = True
                                 else:
                                     balloon = tmp[0]
-                                    part_number = tmp[1]
-                                    balloons[balloon] = part_number
+                                    label_part_number = tmp[1]
+                                    balloons[balloon] = label_part_number
+                        if select == "LABELS":
+                            if ":" in line:
+                                tmp = line.split(":")
+                                if len(tmp) != 2:
+                                    self.pa.error("Error in script in line #"+str(ln))
+                                    error = True
+                                else:
+                                    label_name = tmp[0]
+                                    balloon = tmp[1]
+                                    labels[label_name] = balloon
+                
+                # make tms files from script and generate 
+                if not error:
+                    for label_name, balloon in labels.items():
+                        # get label part number
+                        label_part_number = balloons[balloon]
 
-                                    TO BE CONTINUED...
+                        # get label size
+                        self.cur.execute("SELECT * FROM tms WHERE part_number = '"+label_part_number+"'")
+                        fetched_values = self.cur.fetchall()
+                        if len(fetched_values) > 0:
+                            label_size = fetched_values[0][1]
+                        else:
+                            self.pa.error("Cannot find the size of label part number: "+label_part_number)
+                            error = True
+
+                        # make a label
+                        if not error:
+                            report_labels = []
+                            label_filename = self.labels_location+"/"+product_part_number+"/TMS "+product_part_number+" "+label_part_number+" "+label_size+" "+label_name+".btw" # for example: "Y:\Rafael\Cables\Cable Marking\TMS\R-BA93261A\TMS R-BA93261A R-606100612 1-2 T1.btw"
+                            template_filename = self.lbaels_templates_location+"/TMS TEMPLATE "+label_part_number+" "+label_size+".btw"
+                            if not os.path.isfile(label_filename):
+                                shutil.copy(template_filename, label_filename)
+                                self.pa.print("Created file: "+label_filename)
+                                report_labels.append(label_filename)
+                            else:
+                                os.popen(label_filename)
+                                report_labels.append("TMS "+product_part_number+" "+label_part_number+" "+label_size+" "+label_name+".btw")
+        
+                    # generate html report
+                    if not error:
+                        report_path = self.labels_location+"/"+product_part_number+"/TMS APPROVAL FORM"
+                        report_filename = report_path+"/"+work_order+".html"
+                        if not os.path.isdir(report_path):
+                            os.makedirs(report_path)
+                        file = open(report_filename, 'w')
+                        self.cur.execute("SELECT * FROM products WHERE part_number = '"+product_part_number+"'")
+                        fetched_values = self.cur.fetchall()
+                        if len(fetched_values) > 0:
+                            data = fetched_values[0]
+                            part_number = data[0]
+                            desc = data[1]
+                            parent = data[2]
+                            oreder_number = data[3]
+                            wiring_diagram = data[4]
+                            wiring_diagram_rev = data[5]
+                            drawing = data[6]
+                            drawing_rev = data[7]
+                            bom_rev = data[8]
+                            serial_number_format = data[9]
+                        else:
+                            error = True
+                            self.pa.error("Unexpected error")
+                        if not error:
+                            file.write("<html>\n")
+                            file.write("<html>")
+                            file.write("GENERAL INFORMATION:")
+                            file.write("PART NUMBER: "+part_number+"\n")
+                            file.write("DESCRIPTION: "+desc+"\n")
+                            file.write("ORDER PART NUMBER: "+oreder_number+"\n")
+                            file.write("DRAWING PART NUMBER: "+drawing)
+                            file.write("   REV.: "+drawing_rev+"\n")
+                            file.write("BOM REV.: "+bom_rev+"\n")
+                            file.write("DATE: "+self.pa.today()+"\n")
+                            file.write("------------------------------------------------------")
+                            file.write("PRINTED LABELS")
+                            for lbl in report_labels:
+                                file.write(lbl+"\n")
+                        file.close()
+                        os.popen(report_filename)
+
 
         # if there was an error in the way
         if error:
             self.pa.error("Printing aborted")
+        
+        # restart
+        self.display_database()
+        self.pa.restart()
 
     def inventory(self):
-        pass
+        # get action name
+        choices = ("ADD A TMS BOX", "DELETE A TMS BOX")
+        default = choices[0]
+        ans = self.pa.choose("What action would you like to take?", choices, default)
+        error = False
+
+        # add a tms box
+        if ans == "ADD A TMS BOX":
+            # calculate box id
+            self.cur.execute("SELECT * FROM tms_inventory")
+            fetched_values = self.cur.fetchall()
+            boxid = 1
+            if len(fetched_values) != 0:
+                boxid = fetched_values[-1][0]+1
+            
+            # get box part number
+            part_number = self.pa.input("Insert TMS part number for box #"+str(boxid)).upper()
+
+            # test if box part number is in the database
+            if part_number:
+                self.cur.execute("SELECT * FROM tms WHERE part_number = '"+part_number+"'")
+                fetched_values = self.cur.fetchall()
+                if len(fetched_values) == 0:
+                    error = True
+                    self.pa.error("This TMS is not in the database. To add a TMS to the database go to: "+self.lbaels_templates_location+" and create a new template called: TMS TEMPLATE "+part_number+" LBL_SIZE.btw")
+                else:
+                    self.cur.execute("INSERT INTO tms_inventory (box_number, part_number) VALUES ("+str(boxid)+",'"+part_number+"')")
+                    self.con.commit()
+                    self.pa.print("New TMS BOX added successfully!")
+            else:
+                self.pa.error("Invalid PART NUMBER")
+                self.error = True
+
+        # delete a tms box
+        if ans == "DELETE A TMS BOX":
+            box_number = self.pa.input("Insert BOX ID you want to delete").upper()
+
+            if box_number != "":
+                self.cur.execute("SELECT * FROM tms_inventory WHERE box_number = '"+box_number+"'")
+                fetched_values = self.cur.fetchall()
+                if len(fetched_values) == 0:
+                    error = True
+                    self.pa.error("This box number is not in the database")
+                else:
+                    if self.pa.question("Are you sure you want to delete box #"+str(box_number)+"?"):
+                        self.cur.execute("DELETE FROM tms_inventory WHERE box_number = "+box_number)
+                        self.con.commit()
+                        self.pa.print("Box #"+str(box_number)+" Was deleted from the database")
+                    else:
+                        error = True
+                        self.pa.print("Box #"+str(box_number)+" Was NOT deleted from the database")
+            else:
+                error = True
+                self.pa.error("Invalid box number")
+        
+        self.display_database()
+        self.pa.restart()
 """
     def add(self):
         ## ADD A USER
